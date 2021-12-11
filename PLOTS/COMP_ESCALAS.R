e14b43dfedcf5522f0c98bbba97e9249f40b4bb1
Cao Bittencourt Ferreira <88053259+CaoBittencourtFerreira@users.noreply.github.com>
2021-12-11 14:31:15 -0300

    dsds

--- PLOTS/COMP_ESCALAS.R
+++ PLOTS/COMP_ESCALAS.R
@@ -1,77 +1,106 @@
 # 1. PACOTES --------------------------------------------------------------
-c(
-  'gghighlight', 'ggridges', 'ggthemes', 'viridis', 'patchwork', 'scales', #Visualização de dados
-  'plyr', 'tidyverse' #Manipulação de dados
-) -> pkg
+pkg <- c(
+  'readr'
+  , 'ggthemes'
+  , 'gghighlight'
+  , 'tidyverse'
+)
 
 lapply(pkg, function(x)
   if(!require(x, character.only = T))
   {install.packages(x); require(x)})
 
-# Citação dos pacotes
 # lapply(pkg, function(x)
 #   {citation(package = x)})
 
-# 2. DADOS (E AJUSTES RÁPIDOS) ----------------------------------------------------------------
-# Links
-list(
-  'eq_scales.inter' = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRsrF8OaQGKav6PMpUKtnxMYJheKdti3WDkCql92BpmaXepFUUsYqmIuKuMO5h6gumDo8EyDDJWNs4o/pub?gid=0&single=true&output=csv'
-  # , 'eq_scales.brasil' = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRsrF8OaQGKav6PMpUKtnxMYJheKdti3WDkCql92BpmaXepFUUsYqmIuKuMO5h6gumDo8EyDDJWNs4o/pub?gid=1272641510&single=true&output=csv'
-) -> url.eq_scales
+# 2. DADOS -------------------------------------------------------------------
+# Escalas nacionais e internacionais normalizadas para AA <=> (2,0)
+readr::read_csv(
+  url('https://docs.google.com/spreadsheets/d/e/2PACX-1vRsrF8OaQGKav6PMpUKtnxMYJheKdti3WDkCql92BpmaXepFUUsYqmIuKuMO5h6gumDo8EyDDJWNs4o/pub?gid=0&single=true&output=csv')
+) -> scales_norm
 
-list(
-  'child_cost.inter' = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRsrF8OaQGKav6PMpUKtnxMYJheKdti3WDkCql92BpmaXepFUUsYqmIuKuMO5h6gumDo8EyDDJWNs4o/pub?gid=2118253380&single=true&output=csv'
-  # , 'child_cost.brasil' =  
-) -> url.child_cost
-
-# Data frames
-lapply(
-  url.eq_scales, 
-  function(link){
-    read.csv(
-      url(link), 
-      encoding = 'UTF-8',
-      na.strings = 'NA',
-      header = T, 
-      sep = ','
-    ) %>% as_tibble(.) %>% 
-      mutate(
-        across(.cols = 4:ncol(.), ~ str_replace(.x,',','.')),
-        across(.cols = 4:ncol(.), ~ as.numeric(.x))
-      )
-  }
-) -> list.eq_scales
-
-lapply(
-  url.child_cost, 
-  function(link){
-    read.csv(
-      url(link), 
-      encoding = 'UTF-8',
-      na.strings = 'NA',
-      header = T, 
-      sep = ','
-    ) %>% as_tibble(.) %>% 
-      mutate(
-        across(.cols = 4:ncol(.), ~ str_replace(.x,',','.')),
-        across(.cols = 4:ncol(.), ~ as.numeric(.x))
-      )
-  }
-) -> list.child_cost
+# Custos por criança das escalas nacionais e internacionais
+readr::read_csv(
+  url('https://docs.google.com/spreadsheets/d/e/2PACX-1vRsrF8OaQGKav6PMpUKtnxMYJheKdti3WDkCql92BpmaXepFUUsYqmIuKuMO5h6gumDo8EyDDJWNs4o/pub?gid=1481008835&single=true&output=csv')
+) -> child_cost
 
 
-# 3. TEMAS ----------------------------------------------------------------
-theme_set(theme_solarized_2()) 
+# 3. MANIPULAÇÃO DE DADOS PARA PLOTAGEM -----------------------------------
+scales_norm %>% 
+  pivot_longer(
+    cols = contains('AA')
+    , names_to = 'family_type'
+    , values_to = 'scale'
+  ) %>% select(-A) %>%
+  replace(is.na(.), 0) -> scales_norm
 
-# 4. PLOT ESCALAS DE EQUIVALÊNCIA -----------------------------------------
-# Internacionais
-
-# Nacionais
-
-# Comparação de ambas
-
-
+# child_cost %>% 
+#   pivot_longer(
+#     cols = contains('AA')
+#     , names_to = 'family_type'
+#     , values_to = 'scale'
+#   ) -> scales_norm
 
 
+# 4. VISUALIZAÇÃO (ESCALAS) -----------------------------------------------
+# theme_set(ggthemes::theme_hc(base_size = 25)) 
+theme_set(ggthemes::theme_hc(base_size = 15)) 
 
-# 5. PLOT CUSTO DE CRIANÇA ------------------------------------------------
+scales_norm %>% 
+  mutate(
+    Escala = fct_reorder(
+      Escala, scale, max, .desc = T
+    )
+    , highlight = str_detect(Escala, 'confec')
+  ) %>% 
+  ggplot(
+    aes(
+      x = family_type
+      , y = scale
+      , fill = Região
+    )) + 
+  geom_col() + 
+  # geom_col() + 
+  facet_wrap(facets = vars(Escala)) + 
+  gghighlight(
+    highlight
+    , calculate_per_facet = T
+    , unhighlighted_params = list(fill = NULL, alpha = 0.5)
+    ) +
+  ggthemes::scale_fill_wsj() +
+  coord_flip() + 
+  theme(
+    legend.title = element_blank()
+    , axis.title.x = element_blank()
+    , axis.title.y = element_blank()
+  )
+
+
+
+
+# 3. VISUALIZAÇÃO (CHILD COST) -----------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
